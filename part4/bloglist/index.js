35f863cf8417fd25f565c9fbd0dd8410f098b4cd
aram <almustafa.ahmadrami@gmail.com>
2026-03-01 19:07:29 +0100

    part 4.2: refactored routers

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-const Blog = require('./models/blogs')
+const blogsRouter = require('./controllers/blogs')
+
+// const Blog = require('./models/blog')
 
 require('dotenv').config()
 
@@ -12,19 +14,21 @@ mongoose.connect(process.env.MONGODB_URI, { family: 4 })
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+// app.get('/api/blogs', (request, response) => {
+//   Blog.find({}).then((blogs) => {
+//     response.json(blogs)
+//   })
+// })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+//   blog.save().then((result) => {
+//     response.status(201).json(result)
+//   })
+// })
+
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
