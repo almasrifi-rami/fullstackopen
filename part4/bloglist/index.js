bddf5a50c45ffcea0e8e6b1bab1130c2700c8f1c
aram <almustafa.ahmadrami@gmail.com>
2026-03-01 19:25:10 +0100

    part 4.2: refactored middleware + others

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,36 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogsRouter = require('./controllers/blogs')
-
-// const Blog = require('./models/blog')
-
-require('dotenv').config()
-
-const app = express()
-
-
-mongoose.connect(process.env.MONGODB_URI, { family: 4 })
-
-app.use(express.json())
-
-// app.get('/api/blogs', (request, response) => {
-//   Blog.find({}).then((blogs) => {
-//     response.json(blogs)
-//   })
-// })
-
-// app.post('/api/blogs', (request, response) => {
-//   const blog = new Blog(request.body)
-
-//   blog.save().then((result) => {
-//     response.status(201).json(result)
-//   })
-// })
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
