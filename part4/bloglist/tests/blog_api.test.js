aa42e5bdfa62e2d7f427101a028340d68f4e5d89
aram <almustafa.ahmadrami@gmail.com>
2026-04-06 22:06:53 +0200

    part 4.8-4.12

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,110 @@
+const assert = require('node:assert')
+const { test, describe, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('unique identifier property is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const blog = response.body[0]
+
+  assert(Object.hasOwn(blog, 'id'))
+  assert(!Object.hasOwn(blog, '_id'))
+})
+
+test('blog can be added to the database', async () => {
+  const newBlog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(blog => blog.title)
+  assert(titles.includes('Go To Statement Considered Harmful'))
+})
+
+test('blog likes default to zero', async () => {
+  const blogWithoutLikes = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(blogWithoutLikes)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const addedBlog = blogsAtEnd.find(blog => blog.title === 'Go To Statement Considered Harmful')
+  assert.strictEqual(addedBlog.likes, 0)
+})
+
+describe('blog title and url must exist', () => {
+  
+  test('blog title must exist', async () => {
+    const blogWithoutTitle = {
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogWithoutTitle)
+      .expect(422)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('blog url must exist', async () => {
+    const blogWithoutUrl = {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogWithoutUrl)
+      .expect(422)
+      .expect('Content-Type', /application\/json/)
+  })
+})
+
+
+after(async () => {
+  await mongoose.connection.close()
+})
