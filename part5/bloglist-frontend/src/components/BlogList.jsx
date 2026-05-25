66d77633f8b8a7be3cd10b69b349462ffdfe5981
aram <almustafa.ahmadrami@gmail.com>
2026-05-25 15:51:01 +0200

    part 5.10

--- part5/bloglist-frontend/src/components/BlogList.jsx
+++ part5/bloglist-frontend/src/components/BlogList.jsx
@@ -1,9 +1,11 @@
 import Blog from './Blog'
 
 const BlogList = ({ blogs, blogLike }) => (
-  blogs.map(blog =>
-    <Blog key={blog.id} blog={blog} blogLike={blogLike} />
-  )
+  blogs
+    .sort((a, b) => b.likes - a.likes)
+    .map(blog =>
+      <Blog key={blog.id} blog={blog} blogLike={blogLike} />
+    )
 )
 
 export default BlogList
